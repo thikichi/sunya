09c0b721e1755bc570fc77b9d6553be5609b6ecf
t.hikichi <UserEmail>
2019-03-15 12:42:41 +0900

    PHPバージョン違いによるワーニングを修正

--- lib/metabox/init.php
+++ lib/metabox/init.php
@@ -735,7 +735,6 @@ class cmb_Meta_Box {
 		if ( ! isset( $meta_box['pages'] ) )
 			return self::get_mb_type();
 
-		$type = false;
 		// check if 'pages' is a string
 		if ( self::is_options_page_mb( $meta_box ) )
 			$type = 'options-page';
@@ -743,7 +742,7 @@ class cmb_Meta_Box {
 		elseif ( is_string( $meta_box['pages'] ) )
 			$type = $meta_box['pages'];
 		// if it's an array of one, extract it
-		elseif ( is_array( $meta_box['pages'] ) && count( $meta_box['pages'] === 1 ) )
+		elseif ( is_array( $meta_box['pages'] ) && count( $meta_box['pages'] ) === 1 )
 			$type = is_string( end( $meta_box['pages'] ) ) ? end( $meta_box['pages'] ) : false;
 
 		if ( !$type )
